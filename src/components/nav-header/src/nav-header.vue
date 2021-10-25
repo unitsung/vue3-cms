fe777b4e61b4a357dabee93da935ae091854c9f1
unitsung <unitsung@163.com>
2021-10-25 21:34:36 +0800

    feat: 添加breadcrumb

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -5,23 +5,46 @@
       :class="isFold ? 'el-icon-s-fold' : 'el-icon-s-unfold'"
       @click="handleFoldClick"
     ></i>
+    <div class="content">
+      <hy-breadcrumb :breadcrumbs="breadcrumbs" />
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import UserInfo from './user-info.vue'
+import HyBreadcrumb from '@/base-ui/breadcrumb'
+import { pathMapBreadcrumbs } from '@/utils/mapMenus'
+import { useStore } from '@/store'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   emits: ['foldChange'],
+  components: {
+    UserInfo,
+    HyBreadcrumb
+  },
   setup(props, { emit }) {
     const isFold = ref(false)
     const handleFoldClick = () => {
       isFold.value = !isFold.value
       emit('foldChange', isFold.value)
     }
+
+    // 面包屑数据：[{name:,path:}]
+    const store = useStore()
+    const breadcrumbs = computed(() => {
+      const userMenus = store.state.login.userMenu
+      const currentPath = useRoute().path
+      return pathMapBreadcrumbs(userMenus, currentPath)
+    })
+
     return {
       isFold,
-      handleFoldClick
+      handleFoldClick,
+      breadcrumbs
     }
   }
 })
